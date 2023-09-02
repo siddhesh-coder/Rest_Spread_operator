2f492555bf82801cd6be8ba5c85c8192d1dcc87b
Siddhesh Bhosale <72059095+siddhesh-coder@users.noreply.github.com>
2023-09-02 16:37:43 +0530

    Create eg.js

---
+++ eg.js
@@ -0,0 +1,27 @@
+//REST Operator
+function square(by,...nums){
+    return nums.map((items) => by*items);
+}
+
+const ans = square(2,1,2,3,4,5,6,7,8,9,10);
+console.log(ans);
+
+//SPREAD Operator
+
+const array= [34,56,78];
+function add(num1,num2,num3){
+   const addition = num1+num2+num3;
+   console.log("Addition of three numbers:"+ addition);
+}
+
+add(...array);
+
+//Combination of both REST & SPREAD Operator
+
+const place = ["Mumbai","Pune","Satara","Kolhapur","Goa"];
+
+function visitLog(place1,place2,...restVisit){
+   console.log(`first visit ${place1} then go for place second ${place2} then go for rest of the places ${restVisit}`);
+}
+
+visitLog(...place);
